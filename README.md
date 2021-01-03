0a754232af158edddb9b144191948322fd0dd6b2
Damian Ogrodnik <d.ogrodnik96@gmail.com>
2021-01-03 13:21:11 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Star-Wars-App
+
+## Installing
+
+To get the project running locally you have to:
+
+- Clone this repository `https://github.com/Damian-Ogrodnik/star-wars-app.git`
+- Use `npm install` in project directory to install all required dependencies
+- Use `npm run start` to start the local server (this project uses create-react-app)
+- Open browser on `http://localhost:8080/`
+
+Build:
+
+- `npm run build`
+
+Also you can check [LIVE DEMO](https://damian-ogrodnik.github.io/star-wars-app/) without installing project locally.
--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
 import 'regenerator-runtime/runtime';
 import './styles/main.scss';
 
-import {Error404} from './views/components/Error/Error404';
-import {Loader} from './views/components/Loader';
-import {Navbar} from './views/components/Navbar';
-import {parseRequestURL} from './common/helpers';
-import {routesWithDetails} from './core/routes';
-
-const router = async () => {
-  const navigation = null || document.getElementById('navigation');
-  const content = null || document.getElementById('root');
-
-  navigation.innerHTML = await Navbar.render();
-  await Navbar.after_render();
-
-  content.innerHTML = await Loader.render();
-
-  const request = parseRequestURL();
-
-  const parsedURL =
-    (request.resource ? `/${request.resource}` : '/') +
-    (request.id ? '/:id' : '') +
-    (request.verb ? `/${request.verb}` : '');
-
-  if (!routesWithDetails[parsedURL]) return (content.innerHTML = await Error404.render());
-
-  const {component, fetchData} = routesWithDetails[parsedURL];
-  const data = await fetchData(request.id);
-  content.innerHTML = await component.render(data);
-  await component.after_render(data);
-};
+import {router} from './core/router';
 
 window.addEventListener('hashchange', router);
 window.addEventListener('load', router);
